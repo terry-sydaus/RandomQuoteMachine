62b0c61ff3d4c70e6903494112ed49e831908ef0
Terry Murphy <terryjmurphy71@gmail.com>
2019-04-20 14:55:49 +1000

    Updates

---
+++ precache-manifest.30d870ceb757196847429f01753bf3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3a13b304a01825b085f",
+    "url": "/RandomQuoteMachine/static/js/runtime~main.fbe941b2.js"
+  },
+  {
+    "revision": "0dab9b46a99165573082",
+    "url": "/RandomQuoteMachine/static/js/main.fb238e6d.chunk.js"
+  },
+  {
+    "revision": "85a9204d074aeec95736",
+    "url": "/RandomQuoteMachine/static/js/2.69d903a1.chunk.js"
+  },
+  {
+    "revision": "0dab9b46a99165573082",
+    "url": "/RandomQuoteMachine/static/css/main.2a293d90.chunk.css"
+  },
+  {
+    "revision": "bab5f8dffe0b906f3c01f6f127c46f3e",
+    "url": "/RandomQuoteMachine/index.html"
+  }
+];
